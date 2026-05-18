0ffaec9afa74b1d4532625376d0374b8461daad0
linus <1061558@bkrnet.de>
2026-05-18 08:07:04 +0200

    Projektdateien hinzufügen.

---
+++ Brawlstars_Stats/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Brawlstars_Stats.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
